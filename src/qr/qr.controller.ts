1e849dbaf9b9f041ba46308607269a1f1657fb8e
u632653 <jmgimenez2@teco.com.ar>
2023-06-23 10:04:23 -0300

    generaciÂon de qr en base 64

---
+++ src/qr/qr.controller.ts
@@ -0,0 +1,29 @@
+import { Controller, Post } from '@nestjs/common';
+import { ApiOperation } from '@nestjs/swagger';
+import QRCode = require('qrcode');
+import { v4 as uuidv4 } from 'uuid';
+
+@Controller('qr')
+export class QrController {
+  @Post()
+  @ApiOperation({ summary: 'Crear nuevo punto de venta' })
+  async createQr() {
+    try {
+      console.log('sadas');
+      const data = {
+        id: uuidv4(),
+        name: 'Jorge Gimenez',
+        curso: 'NestJS',
+        url: 'https://www.npmjs.com/package/qrcode#usage',
+      };
+      const strJson = JSON.stringify(data);
+      const config = { type: 'terminal' };
+      const qr = await QRCode.toDataURL(strJson, config);
+      console.log(qr);
+      return {
+        status: 200,
+        msj: 'QR GENERADO',
+      };
+    } catch (error) {}
+  }
+}
