a487cf7300309342fd87b7343416fcceeda63743
u632653 <jmgimenez2@teco.com.ar>
2023-06-30 10:53:38 -0300

    puertos nuevos

--- src/main.ts
+++ src/main.ts
@@ -3,7 +3,7 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  const port = 65002;
+  const port = 8888;
   await app.listen(port, () => {
     console.log('🚀 Application is running on:  http://localhost:' + port);
   });
